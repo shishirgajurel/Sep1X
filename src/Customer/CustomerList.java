9d07d0ae2c6f47ec8b4a210105346b91e9abb5da
DanWeld <354818@via.dk>
2024-11-26 23:55:59 +0100

    Update CustomerList.java

--- src/Customer/CustomerList.java
+++ src/Customer/CustomerList.java
@@ -2,4 +2,68 @@ package Customer;
 
 public class CustomerList
 {
+
+  private<Customer> customers;
+
+    // Constructor
+    public CustomerList()
+    {
+        customers = new<Customer>();
+    }
+// get number of customers
+    public int getNumberOfCustomers()
+    {
+        return customers.size();
+    }
+
+    //get all customers
+    public<Customer> getCustomers()
+    {
+        return customers;
+    }
+
+    // add customer
+    public void addCustomer(Customer customer)
+    {
+        customers.add(customer);
+    }
+    // get customer from customer list
+    public Customer getCustomer(Customer customer)
+    {
+        return customers.get(customers.indexOf(customer));
+    }
+    // remove customer
+    public void removeCustomer(Customer customer)
+    {
+        customers.remove(customer);
+    }
+   
+    // toString
+    public String toString()
+    {
+        return "CustomerList{" +
+                "customers=" + customers +
+                '}';
+    }
+    // equals
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass())
+        {
+            return false;
+        }
+        CustomerList that = (CustomerList) obj;
+        return customers.equals(that.customers);
+    }
+
+
+
+
+    
+
+
 }
